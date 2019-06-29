b0ace9508df6175d20f848f09b3762022fc7b993
xiaodong.zou <zxd_0825@163.com>
2019-06-29 21:41:36 +0800

    stream test

--- src/main/java/com/zxd/java8/test/lambda/Distinct.java
+++ src/main/java/com/zxd/java8/test/lambda/Distinct.java
@@ -2,10 +2,17 @@ package com.zxd.java8.test.lambda;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class Distinct {
     public static void main(String[] args) {
-        List<Integer> numbers = Arrays.asList(1, 2, 1, 3, 3, 2, 4);
-        numbers.stream().filter(i -> i%2 == 0).distinct().forEach(System.out::println);
+        List<Integer> numbers = Arrays.asList(1, 2, 1,6, 3, 3, 2, 4);
+        numbers.stream().filter(i -> i%2 == 0).distinct().limit(2).forEach(System.out::println);
+        numbers.stream().filter(i -> i%2 == 0).distinct().skip(2).forEach(System.out::println);
+        List<String> words = Arrays.asList("Java 8", "Lambdas", "In", "Action");
+        List<Integer> lengths = words.stream().map(e -> e.length()).collect(Collectors.toList());
+        System.out.println(lengths);
+        List<Integer> test = words.stream().map(String::length).collect(Collectors.toList());
+        System.out.println(test);
     }
 }
